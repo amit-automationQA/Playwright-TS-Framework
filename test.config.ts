6409f6b59d115df7b17d8c882dba6dc78973506e
amit-automationQA <kundaramit@gmail.com>
2026-01-17 15:40:05 +0530

    Modified Test Config

--- test.config.ts
+++ test.config.ts
@@ -1,4 +1,5 @@
 export class TestConfig {
+  // Configuration settings for test automation
   static readonly APP_URL = 'https://testautomationpractice.blogspot.com/';
   static readonly TIMEOUT = 5000; // in milliseconds
   static readonly LOG_LEVEL = 'debug';
